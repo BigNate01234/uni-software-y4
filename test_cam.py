eab777c3756d95bd462511bfee5f9c014da3ebe9
Nathaniel Howes <nathan.khowes@gmail.com>
2025-11-16 16:35:32 +0000

    updating test script

--- test_cam.py
+++ test_cam.py
@@ -1,19 +1,20 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
 if not cap.isOpened():
-    print("Camera FAILED to open!")
+    print("Could not open camera!")
     exit()
 
-print("Camera opened OK. Press q to quit.")
+print("Camera opened successfully. Press q to quit.")
 
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("Cannot read frame!")
+        print("Could not read frame!")
         break
 
-    cv2.imshow("Test Camera", frame)
+    cv2.imshow("Camera Test", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
